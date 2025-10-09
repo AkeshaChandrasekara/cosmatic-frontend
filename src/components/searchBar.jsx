4e5d2458d61d0472b9a2b8095865d9215b5f411f
gthilakshana <gavrawavanniarachchi@gmail.com>
2025-10-09 15:45:07 +0530

    searchBar Updated

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -2,13 +2,13 @@ import { FaSearch } from "react-icons/fa";
 
 export default function SearchBar({ placeholder = "Search products..." }) {
     return (
-        <div className="relative  max-w-xs md:max-w-sm w-full md:w-[350px] rounded-md">
+        <div className="relative max-w-xs md:max-w-sm w-full md:w-[350px] ">
             <input
                 type="text"
                 placeholder={placeholder}
-                className="w-full border bg-gray-50 border-gray-50 text-black rounded-md pl-4 pr-10 py-2 text-sm focus:outline-none  focus:border-white transition duration-200"
+                className="w-full border bg-gray-50 border-gray-50 text-black rounded-full pl-4 pr-10 py-2 text-sm focus:outline-none  focus:border-white transition duration-200"
             />
-            <FaSearch className="absolute right-3 top-2.5 text-green-900" size={16} />
+            <FaSearch className="absolute right-3 top-2.5 text-green-600" size={16} />
         </div>
     );
 }
