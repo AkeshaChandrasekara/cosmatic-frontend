9f532327ae392fce9c75557f9f6a92a934945e01
Akesha <akeshanawanjali23@gmail.com>
2025-11-24 21:37:22 +0530

    mediaupload.jsx updated📌

--- utils/mediaUpload.jsx
+++ utils/mediaUpload.jsx
@@ -1,31 +1,32 @@
 import { createClient } from "@supabase/supabase-js";
 
-const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrZGJldG1uZXJlbXFhYWd4Y2J2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0Njc2MjEsImV4cCI6MjA3MjA0MzYyMX0.97PJmhCdhUSegf1m7fqqUtsRDlY04EpDV3qjMgfr5UI`;
-const url = "https://ckdbetmneremqaagxcbv.supabase.co";
+const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhocnhucGppcHJ6bHRrcHBlZ3d0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3NTQ3NTcsImV4cCI6MjA2NjMzMDc1N30.RTKy9gU7uiEodV1CKIgeJnKBQcbNpg0jdsOD-xYwlNY`
+
+const url = "https://xhrxnpjiprzltkppegwt.supabase.co"
+
 
 const supabase = createClient(url, key);
 
-
 export default function uploadMediaToSupabase(file) {
-    return new Promise((resolve, reject) => {
-        if (file == null) {
-            reject("File not added");
-        }
-        let fileName = file.name;
-        const extension = fileName.split(".")[fileName.split(".").length - 1];
+  return new Promise((resolve, reject) => {
+    if (file == null) {
+      reject("File not added");
+    }
+    let fileName = file.name;
+    const extension = fileName.split(".")[fileName.split(".").length - 1];
 
-        const timestamp = new Date().getTime();
+    const timestamp = new Date().getTime();
 
-        fileName = timestamp + file.name + "." + extension;
+    fileName = timestamp +file.name+ "." + extension;
 
-        supabase.storage.from("images").upload(fileName, file, {
-            cacheControl: "3600",
-            upsert: false,
-        }).then(() => {
-            const publicUrl = supabase.storage.from("images").getPublicUrl(fileName).data.publicUrl;
-            resolve(publicUrl);
-        }).catch((err) => {
-            reject(err);
-        });
+    supabase.storage.from("cosmetics").upload(fileName, file, {
+      cacheControl: "3600",
+      upsert: false,
+    }).then(()=>{
+      const publicUrl = supabase.storage.from("cosmetics").getPublicUrl(fileName).data.publicUrl;
+      resolve(publicUrl);
+    }).catch((err)=>{
+      reject(err);
     });
+  });
 }
